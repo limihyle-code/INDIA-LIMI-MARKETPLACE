8547916f1bb7dc2961f9146685ec1e63e607a618
limihyle-code <limihyle@gmail.com>
2026-08-24 14:12:22 +0530

    Remove unused import 'send_from_directory' from newfile.py

--- newfile.py
+++ newfile.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template_string, jsonify, send_from_directory
+from flask import Flask, request, redirect, render_template_string, jsonify
 import requests
 import time
 import os
